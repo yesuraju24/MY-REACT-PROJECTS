4c5d9feb1ad19932de1e47cfe847d75de846dfda
Yesuraju M <madigarajumadigaraju@gmail.com>
2024-06-18 12:05:14 +0530

    first commit

---
+++ my-react-app/src/App.js
@@ -0,0 +1,17 @@
+
+import React  from "react"   
+import "./index.css" 
+
+
+const App = () =>{   
+  
+  return(  
+    <div>
+      <h1>Hello Hooks Raju</h1> 
+    </div>
+  )   
+}    
+export default App 
+
+
+ 
---
+++ my-react-app/src/index.js
@@ -0,0 +1,12 @@
+import React from "react" 
+import reactDOM from "react-dom/client" 
+import App from './App' 
+import "./index.css"
+
+const root = reactDOM.createRoot(document.getElementById("root")) 
+
+root.render(
+    <> 
+    <App/>
+    </>
+)
